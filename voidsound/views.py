e191ef62a80bf703cccc7457f0264a5329d2a73e
Pablo Flores <andresflores200025@gmail.com>
2026-06-14 14:15:34 -0500

    Ya no me acuerdo de que serán estos cambios

--- voidsound/views.py
+++ voidsound/views.py
@@ -37,15 +37,14 @@ class ArtistaUpdateView(UpdateView):
         id_nueva = form.cleaned_data.get('id_artista')
 
         if id_nueva and id_antigua != id_nueva:
-            # 1. Guardar el nuevo artista con la ID nueva
+
             nuevo_objeto = form.save(commit=False)
             nuevo_objeto.pk = id_nueva
             nuevo_objeto.save()
 
-            # 2. Mover todos los álbumes del artista viejo al nuevo ID para evitar el ProtectedError
+
             Album.objects.filter(artista_id=id_antigua).update(artista_id=id_nueva)
 
-            # 3. Ahora que el ID viejo está libre y sin hijos, lo podemos borrar de forma segura
             Artista.objects.filter(pk=id_antigua).delete()
             return redirect(self.success_url)
 
@@ -59,8 +58,7 @@ class ArtistaDeleteView(DeleteView):
     def form_valid(self, form):
         artista = self.get_object()
 
-        # Cargamos los modelos dinámicamente apuntando a la app 'user'
-        # (Si tu app se llama 'usuarios' en Django, cambia 'user' por 'usuarios')
+
         Reproduccion = apps.get_model('user', 'Reproduccion')
         Likes = apps.get_model('user', 'Likes')
         CancionPlaylist = apps.get_model('user', 'CancionPlaylist')
@@ -70,7 +68,7 @@ class ArtistaDeleteView(DeleteView):
             canciones = Cancion.objects.filter(album_id__in=albumes_ids)
             canciones_ids = canciones.values_list('pk', flat=True)
 
-            # Ahora Python ya reconocerá las variables perfectamente:
+
             Reproduccion.objects.filter(cancion_id__in=canciones_ids).delete()
             Likes.objects.filter(cancion_id__in=canciones_ids).delete()
             CancionPlaylist.objects.filter(cancion_id__in=canciones_ids).delete()
@@ -154,9 +152,7 @@ class CancionUpdateView(UpdateView):
             nuevo_objeto.pk = id_nueva
             nuevo_objeto.save()
 
-            # Las canciones comúnmente no tienen tablas hijas directas (a menos que dependan de listas de reproducción),
-            # si tu tabla 'Likes' o 'Playlist_Cancion' usa 'id_cancion', deberías actualizarla aquí de forma similar:
-            # Likes.objects.filter(cancion_id=id_antigua).update(cancion_id=id_nueva)
+
 
             Cancion.objects.filter(pk=id_antigua).delete()
             return redirect(self.success_url)
@@ -195,7 +191,7 @@ class GeneroUpdateView(UpdateView):
             nuevo_objeto.pk = id_nueva
             nuevo_objeto.save()
 
-            # Mover todas las canciones vinculadas a este género hacia el nuevo ID
+
             Cancion.objects.filter(genero_id=id_antigua).update(genero_id=id_nueva)
 
             Genero.objects.filter(pk=id_antigua).delete()
